5ca341aa07a1f0e875cd74a8b1637744d7836060
Manu Mahadevan Koipallil <46205033+manukoip@users.noreply.github.com>
2020-02-02 22:39:25 -0800

    Add files via upload

---
+++ public/javascripts/getWeather.js
@@ -0,0 +1,67 @@
+// Outputs reponse to page with given class name and message
+// if output element exists
+function outputResponse(className, message) {
+  var outputElement = document.querySelector("#response-output");
+
+  if (!outputElement) {
+    return;
+  }
+
+  outputElement.classList = className;
+  outputElement.innerHTML = message;
+}
+
+// Handles the response of the weather API call
+function handleFormResponse(data) {
+  if (data.weather) {
+    // TODO: Add temperature to response output
+    var description = data.weather[0].description;
+    var temperature = data.main.temp.toFixed(1);
+
+    var weatherOutput = `${temperature} ${description}`;//`${description}` CHANGED TO ADD REAL TIME TEMPERATURE
+
+    outputResponse("weather", weatherOutput);
+  } else {
+    var errorOutput = data.message;
+
+    outputResponse("error", errorOutput);
+  }
+}
+
+// Returns the value of the city name input if it exists
+// (returns empty string otherwise)
+function getCityInputValue() {
+  var cityInputElement = document.querySelector("input[name='city']");
+
+  if (!cityInputElement) {
+    return "";
+  }
+
+  return cityInputElement.value;
+}
+
+// Handles form submission by making API call to weather endpoint
+// with given city name
+function handleFormSubmit(event) {
+  event.preventDefault();
+  document.activeElement.blur();
+  outputResponse("Loading"," ... ");
+
+  // TODO: Add loading state output
+  fetch(`/weather/${getCityInputValue()}`, {
+    method: "get",
+    headers: { "Content-Type": "application/json" }
+  })
+    .then(r => r.json())
+    .then(data => handleFormResponse(data));
+}
+
+// Creates event listener for form submission once the page
+// has loaded and form element exists
+document.addEventListener("DOMContentLoaded", function() {
+  var weatherFormElement = document.querySelector("#weather-form");
+
+  if (weatherFormElement) {
+    weatherFormElement.addEventListener("submit", handleFormSubmit);
+  }
+});
